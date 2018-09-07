a2e9143d5d405328504df674101427e05f2f4971
Scott Wilson <scott.wilson@voiteq.com>
2018-09-07 10:13:53 +0100

    It's not pretty, but it works well

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) {
     updateImage();
 });
 
@@ -11,12 +11,41 @@ function updateImage() {
     // clear messages
     writeMessage("");
     var canvas = document.getElementById("output");
-    
+
     // get data points
-    parseInput(document.getElementById("txtInput").innerHTML);
+    var inputData = parseInput(document.getElementById("txtInput").value);
+    if (inputData.error) {
+        writeMessage(inputData.error);
+        return;
+    }
+
+    // Get output size
+    var pixelSize = document.getElementById("pixelSize").value;
+
+    // Reformat canvas
+    canvas.width = inputData.width * pixelSize;
+    canvas.height = inputData.height * pixelSize;
+
+    var ctx = canvas.getContext("2d");
+
+    for (let y = 0; y < inputData.height; y++) {
+        for (let x = 0; x < inputData.width; x++) {
+            var r,g,b;
+            if (inputData.type === "P3") {
+                r = 255 * inputData.pixelData[3 * (y * inputData.width + x)] / inputData.maxValue;
+                g = 255 * inputData.pixelData[3 * (y * inputData.width + x) + 1] / inputData.maxValue;
+                b = 255 * inputData.pixelData[3 * (y * inputData.width + x) + 2] / inputData.maxValue;
+            } else {
+                r = g = b = 255 * inputData.pixelData[y * inputData.width + x] / inputData.maxValue;
+            }
+
+            ctx.fillStyle = `rgb(${r},${g},${b})`;
+            ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+        }
+    }
 }
 
-function parseInput(rawText){
+function parseInput(rawText) {
     var lines = rawText.split("\n");
     var dataPoints = [].concat(...lines.map(l => {
         var precomment = l.split("#")[0];
@@ -24,5 +53,47 @@ function parseInput(rawText){
         return dataPointSet;
     }));
 
-    console.log(dataPoints);
+    // Check type is supported
+    var type = dataPoints.shift();
+    if (["P4", "P5", "P6", "P7"].includes(type)) {
+        return {
+            error: "Binary NetPbm formats unsupported"
+        };
+    } else if (!["P1", "P2", "P3"].includes(type)) {
+        return {
+            error: `Header type ${type} unrecognised`
+        };
+    }
+
+    // Get output size
+    var width = parseInt(dataPoints.shift());
+    var height = parseInt(dataPoints.shift());
+
+    // If not P1, also get maxValue
+    var maxValue = 1;
+    if (type !== "P1") {
+        maxValue = parseInt(dataPoints.shift());
+    }
+
+    var pixelData = dataPoints.map(p => parseInt(p));
+
+    // If P1, invert 1s and 0s
+    if (type === "P1") {
+        pixelData = pixelData.map(p => 1 - p);
+    }
+
+    //Should have correct number of data points remaining
+    if (pixelData.length !== width * height * (type === "P3" ? 3 : 1)) {
+        return {
+            error: `Invalid number of pixel values - expected ${width * height * (type === "P3" ? 3 : 1)}, actual ${pixelData.length}`
+        };
+    }
+
+    return {
+        type: type,
+        width: width,
+        height: height,
+        maxValue: maxValue,
+        pixelData: pixelData
+    };
 }
